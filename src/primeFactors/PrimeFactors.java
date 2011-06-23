bc0579367eaeb668a0b6883f5d968ea0bd97494d
Stephen Kuenzli <skuenzli@gmail.com>
2011-06-22 20:04:58 -0700

    Added comment.

--- src/primeFactors/PrimeFactors.java
+++ src/primeFactors/PrimeFactors.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * PrimeFactors is responsible for
+ * PrimeFactors is responsible for finding the prime factors of arbitrary numbers.
  *
  * @author skuenzli
  */
